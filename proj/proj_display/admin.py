8a3e80cb7eddd422bd3c3d6e5b2b8de6af0b6e0f
lynchej <lynchej>
2024-10-13 10:12:04 -0400

    Revert "adding fiscal facts"
    This reverts commit b8dedefd

--- proj/proj_display/admin.py
+++ proj/proj_display/admin.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from .models import SchoolFastFacts
 from .models import DistrictFastFacts
-from .models import School
-from .models import District
-from .models import County
 
 # Register your models here.
 admin.site.register(SchoolFastFacts)
 admin.site.register(DistrictFastFacts)
-admin.site.register(School)
-admin.site.register(District)
-admin.site.register(County)
