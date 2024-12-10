af3c938914263f71bf6d13f44083b20ff693bab6
lynchej <lynchej>
2024-12-10 17:58:15 -0500

    removed school districts from the data in graduate by public school

--- data/GraduatesPublicSchool/clean_grad_dis.py
+++ data/GraduatesPublicSchool/clean_grad_dis.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import os
+
 
 years=['2007-08', '2008-09', '2009-10', '2010-11', '2011-12', '2012-13', '2013-14', '2014-15', '2015-16', '2016-17', '2017-18', '2018-19', '2019-20', '2020-21', '2021-22', '2022-23']
 
@@ -25,34 +25,7 @@ column_mapping = {
     'specialized associate degree granting institution': ['specialized associate degree granting institution', 'specialized_associate_degree_institution','specialized associate degree-granting institution'],
     'specialized associate degree granting institution %': ['specialized associate degree granting institution %', 'specialized_associate_degree_institution_percentage']
 }
-def standardize_columns(df):
-    # Iterate over the column mapping and rename columns
-    for standard_name, variations in column_mapping.items():
-        for variation in variations:
-            if variation.lower() in df.columns:
-                print(f"Renaming column '{variation.lower()}' to '{standard_name}'")
-                df.rename(columns={variation.lower(): standard_name}, inplace=True)
-    return df
-
-    
-for year in years:
-    df=pd.read_csv(f'/Users/ericlynch/cs320/dbms_proj/data/GraduatesPublicSchool/raw_school_csv/GraduatesPublicbySchool{year}.csv')
-   
-
-    df.columns = df.columns.str.lower()#.str.replace(' ', '_').str.replace('-', '_')
-    for column in df.columns:
-        print(column)
-    df=standardize_columns(df)
-
-    matching_columns = [col for col in standard_columns if col in df.columns]
-    df = df[matching_columns]
-    #new_df=df[df['school number']== 9999]
-    df.drop(df[df['school number']== 9999].index, inplace = True)
-   
-   
-    df.to_csv(f'/Users/ericlynch/cs320/dbms_proj/data/GraduatesPublicSchool/cleaned_school_csv/grad_pub_school_clean_{year}.csv')
-   
 
 
-
-
+df=pd.read_excel(f'/Users/ericlynch/cs320/dbms_proj/data/GraduatesPublicSchool/GraduatesPublicbySchool2008-09.xls',sheet_name=4,skiprows=3)
+print(df.columns)
